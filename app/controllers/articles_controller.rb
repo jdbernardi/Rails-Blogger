652d52557ac5e920cd9d45205e1802c1fd3b65eb
Joe Bernardi <joebernardi@hotmail.com>
2016-10-22 22:26:03 -0500

    fix destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,4 +30,29 @@ class ArticlesController < ApplicationController
 
 	end
 
+
+	def update
+	  @article = Article.find(params[:id])
+	  @article.update(article_params)
+
+	  redirect_to article_path(:index)
+	end
+
+	def destroy
+
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to action: 'index'
+
+
+	end
+
+	def edit
+
+		@article = Article.find(params[:id])
+
+	end
+
+
 end
